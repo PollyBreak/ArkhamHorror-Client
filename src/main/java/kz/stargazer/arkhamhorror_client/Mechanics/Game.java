1309baf38f262929ec5b56e08609e3d0890c95af
PollyBreak <mspolinko@gmail.com>
2023-10-15 21:16:28 +0600

    Update Game.java

--- src/main/java/kz/stargazer/arkhamhorror_client/Mechanics/Game.java
+++ src/main/java/kz/stargazer/arkhamhorror_client/Mechanics/Game.java
@@ -1,4 +1,17 @@
 package kz.stargazer.arkhamhorror_client.Mechanics;
 
+import kz.stargazer.arkhamhorror_client.Heroes.Investigator;
+
+import java.util.ArrayList;
+
 public class Game {
+    private ArrayList<Investigator> players = new ArrayList<>();
+
+
+    public ArrayList<Investigator> getPlayers() {
+        return players;
+    }
+    public void setPlayers(ArrayList<Investigator> players) {
+        this.players = players;
+    }
 }
