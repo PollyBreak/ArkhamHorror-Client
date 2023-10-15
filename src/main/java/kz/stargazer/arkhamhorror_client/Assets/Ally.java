0cd0bf9828cce756966e8546eaaeea52bda0228d
PollyBreak <mspolinko@gmail.com>
2023-10-15 21:15:09 +0600

    added ally

---
+++ src/main/java/kz/stargazer/arkhamhorror_client/Assets/Ally.java
@@ -0,0 +1,14 @@
+package kz.stargazer.arkhamhorror_client.Assets;
+
+import kz.stargazer.arkhamhorror_client.Heroes.Investigator;
+
+public class Ally extends AssetCard {
+    private int health;
+    private int sanity;
+    private Action actionStrategy;
+
+    @Override
+    public void use(Investigator investigator) {
+        actionStrategy.use(investigator);
+    }
+}
