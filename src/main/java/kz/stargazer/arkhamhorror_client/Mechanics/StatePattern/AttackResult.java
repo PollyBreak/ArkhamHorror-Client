ad0bddbba324bf43a31f8ba8d30c4b39bbc3e5c0
PollyBreak <mspolinko@gmail.com>
2023-10-31 21:14:47 +0600

    1

--- src/main/java/kz/stargazer/arkhamhorror_client/Mechanics/StatePattern/AttackResult.java
+++ src/main/java/kz/stargazer/arkhamhorror_client/Mechanics/StatePattern/AttackResult.java
@@ -5,7 +5,7 @@ import kz.stargazer.arkhamhorror_client.Heroes.Investigator;
 public class AttackResult extends ActionResult {
     @Override
     public void act() {
-        
+
     }
 
     public AttackResult(Investigator player, ActionResult nextResult) {
